5e05d4d39a0b419717348937bbc27ce2e7e0d26f
Mykola_Kokoiev <116379690+neagarr@users.noreply.github.com>
2025-11-05 21:30:14 +0100

    Change URL for cookie upload API
    Updated the URL for sending cookies to the new service endpoint.

--- send_cookies_api.py
+++ send_cookies_api.py
@@ -3,7 +3,7 @@ import requests
 
 
 def send_cookies_api(cookies_path, service_id):
-    url = "http://srv10.mikr.us:30149/upload_cookies"
+    url = "http://stories.editio.app/upload_cookies"
     # Download cookie from file
     with open(cookies_path, "r", encoding="utf-8") as f:
         cookies = json.load(f)
